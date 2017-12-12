a000169893619f58c128772a2f3c00812238c372
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-12 20:06:38 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    binding.pry
     @student = Student.new
     @student.first_name = params[:first][:name]
     @student.last_name = params[:last][:name]
